890966808fe8cec067e64749feb434adaff57bc7
Dsteelz <91301867+Dsteelz@users.noreply.github.com>
2022-01-12 18:26:07 -0800

    Initial Commit

---
+++ Instaclone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instaclone
+//
+//  Created by Jairef Fowler on 1/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
