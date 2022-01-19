2203b5c29f357d1440fea708778a4482adb93c17
Dsteelz <91301867+Dsteelz@users.noreply.github.com>
2022-01-18 20:35:24 -0800

    Instaclone project.

--- Instaclone/AppDelegate.swift
+++ Instaclone/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let configuration = ParseClientConfiguration {
+          $0.applicationId = "wnZFPwUmPBYUO2cNHRbDBNm58Uft2AYshTVzdyHa"
+          $0.clientKey = "PQNWAqFbQX80nDDwoOj0S8n7oG4aE5Df9ugDE0Rk"
+          $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: configuration)
+        
         return true
     }
 
